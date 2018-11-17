f281aacb236afc429a9c7b59fe49edd91b300a5d
artsyu <ellieyhc@gmail.com>
2018-11-17 12:48:12 -0800

    Basic adding and subtracting

--- SwipeLeft/app/src/main/java/com/aiyaa/swipeleft/MainActivity.java
+++ SwipeLeft/app/src/main/java/com/aiyaa/swipeleft/MainActivity.java
@@ -2,6 +2,13 @@ package com.aiyaa.swipeleft;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +16,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button subtractButton = findViewById(R.id.subtract);
+        subtractButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView swipes = findViewById(R.id.swipeCounter);
+                String currentCounter = swipes.getText().toString();
+                int count = Integer.parseInt(currentCounter);
+
+                if(count > 0)
+                    swipes.setText("" + --count);
+                else
+                    Toast.makeText(getApplicationContext(), "You don't have any swipes left already!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button addButton = findViewById(R.id.add);
+        addButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView swipes = findViewById(R.id.swipeCounter);
+                String currentCounter = swipes.getText().toString();
+                int count = Integer.parseInt(currentCounter);
+
+                swipes.setText("" + ++count);
+                Toast.makeText(getApplicationContext(), "wHY DId u aDd A sWIpE", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
 
